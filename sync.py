9360991c5480d199b0311272ea3bea0ac2706995
Jonathan Bennett <jdbennet@ca.ibm.com>
2018-12-20 17:33:33 -0500

    Walk both directories

--- sync.py
+++ sync.py
@@ -1,5 +1,6 @@
 from sys import argv
 from os  import scandir
+from os import path
 
 source: str = '/Volumes/Passport/comics'
 target: str = '/Volumes/Public/webbox'
@@ -14,18 +15,43 @@ def scan(directory):
         if entry.is_dir():
             dir:str = entry.path
             print('.. scanning.. ', dir)
-            files += scan(dir)
+            if ( isArchiveDir(dir) ):
+                files += scan(dir)
 
         if entry.is_file():
             files.append(entry.path)
 
     return files
 
+def chompLeft(string, prefix):
+    if string.startswith(prefix):
+        prefix_length: int = len(prefix)
+        return prefix_length[prefix_length:]
+
+def isArchiveDir(directory):
+    return not 'yacreader' in directory
+
+def filterArchives(archive):
+    isArchive:bool =  (archive.endswith('.cbz') or archive.endswith('.cbr'))
+    yacreader:bool = '.yacreader' in archive
+    isHidden:bool = path.basename(archive).startswith('.') or path.basename(archive).startswith('_')
+    return isArchive and not yacreader and not isHidden
 
 source_files = scan(source)
-target_files = scan(target)
+source_files = list(filter(filterArchives, source_files))
 
-print( len(source_files), ' source files scanned, ', len(target), ' target files scanned.')
+for file in source_files:
+    print('...', file)
+
+print( len(source_files), 'files scanned.')
+
+target_files = scan(target)
+target_files = list(filter(filterArchives, target_files))
+
+for file in target_files:
+    print('...', file)
+
+print( len(source_files), ' source files scanned, ', len(target_files), ' target files scanned.')
 
 
 
