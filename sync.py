0696e00c691e34af2d41fc1bce3fa4cbd526103d
Jonathan Bennett <jdbennet@ca.ibm.com>
2018-12-22 09:27:56 -0500

    Diff lists code

--- sync.py
+++ sync.py
@@ -1,6 +1,7 @@
 from sys import argv
 from os  import scandir
 from os import path
+from shutil import copyfile
 
 source: str = '/Volumes/Passport/comics'
 target: str = '/Volumes/Public/webbox'
@@ -26,7 +27,7 @@ def scan(directory):
 def chompLeft(string, prefix):
     if string.startswith(prefix):
         prefix_length: int = len(prefix)
-        return prefix_length[prefix_length:]
+        return string[prefix_length:]
 
 def isArchiveDir(directory):
     return not 'yacreader' in directory
@@ -37,23 +38,29 @@ def filterArchives(archive):
     isHidden:bool = path.basename(archive).startswith('.') or path.basename(archive).startswith('_')
     return isArchive and not yacreader and not isHidden
 
+
+def diffLists(list1, list2):
+    diff = [aa for aa in list1 if aa not in list2]
+    return diff
+
 source_files = scan(source)
 source_files = list(filter(filterArchives, source_files))
-
-for file in source_files:
-    print('...', file)
-
-print( len(source_files), 'files scanned.')
+source_relative = [chompLeft(p, source) for p in source_files]
 
 target_files = scan(target)
 target_files = list(filter(filterArchives, target_files))
-
-for file in target_files:
-    print('...', file)
+target_relative = [chompLeft(p, target) for p in target_files]
 
 print( len(source_files), ' source files scanned, ', len(target_files), ' target files scanned.')
 
 
+delta = diffLists(source_relative, target_relative)
+
+for datum in delta:
+    print('-- > ', datum)
+
+print( 'delta..', len(delta), ' potential inserts')
 
 
-# print( sys.argv )
+
+
